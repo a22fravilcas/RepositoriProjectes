ae43562cb8f48035dff7d1025076187e5125e3c9
sonnymccammond <>
2023-01-10 13:28:00 +0100

    Commit Wilson

--- ProjecteLoteria/src/projecteloteria/ProjecteLoteria.java
+++ ProjecteLoteria/src/projecteloteria/ProjecteLoteria.java
@@ -1,9 +1,19 @@
 package projecteloteria;
 
-public class ProjecteLoteria {
+import utilities.Utilities;
+import java.util.Random;
+import java.util.Scanner;
 
+public class ProjecteLoteria {
+    
+    static Scanner scan = new Scanner(System.in); 
+    static Random rndm = new Random();
+    
     public static void main(String[] args) {
+        
+        
 
     }
     
+    static void NumeroPremiat
 }
