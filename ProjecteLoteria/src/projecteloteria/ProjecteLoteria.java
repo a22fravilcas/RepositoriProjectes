7a49758637b8b334fedc05a9b50b4db959a73dad
sonnymccammondasher <>
2023-01-23 12:06:59 +0100

    Commit sonny

--- ProjecteLoteria/src/projecteloteria/ProjecteLoteria.java
+++ ProjecteLoteria/src/projecteloteria/ProjecteLoteria.java
@@ -25,14 +25,33 @@ public class ProjecteLoteria {
         int numeroUsuari = Utilities.demanaNumEnter("Introdueix el teu numero de loteria. "
                                                     + "El numero ha de ser de cinc digits");
         
+        
+        boolean NumeroTrobat = TrobarNumeroPremiat(array_NumerosPremiats, numeroUsuari);
+        
+        if (!NumeroTrobat) {
+            long premiTrobat = TrobarPremi(indexnummatch, array_Premis);
+            System.out.println("Enhorabona, has aconeguit un premi principal. El teu premi es de " +  premiTrobat + " €.");
+        } else {
+            System.out.println("El teu numero no correspon a cap premi principal.");
+        }
     }
-
+    
+    /**
+     * 
+     * @param premis 
+     */
+    //Funcio que genera un array de numeros possibles premiats
     public static void NumeroPremiat(int premis[]) {
         for (int i = 0; i != premis.length; i++) {
             premis[i] = rndm.nextInt(99999);
         }
     }
-
+    
+    /**
+     * 
+     * @param array_Premis 
+     */
+    //Funcio que assigna el valor a l'array de premis
     public static void CompletarPremis(long array_Premis[]) {
 
         final int TOTALPADREO = 1807;
@@ -60,7 +79,14 @@ public class ProjecteLoteria {
 
     }
 
-    public static boolean MainPrizeMatch(int array_NumerosPremiats[], int numeroUsuari){
+    /**
+     * 
+     * @param array_NumerosPremiats
+     * @param numeroUsuari
+     * @return 
+     */
+    //Funcio que busca si el numero de l'usuari correspon a un dels numeros premiats
+    public static boolean TrobarNumeroPremiat(int array_NumerosPremiats[], int numeroUsuari){
         
         boolean result = false;
         int i = 0;
@@ -78,12 +104,18 @@ public class ProjecteLoteria {
         return result;
     }
     
-    public static int E(int indexnummatch){
+    /**
+     * 
+     * @param indexnummatch
+     * @param premis
+     * @return 
+     */
+    //Funcio que troba el premi corresponent al numero premiat
+    public static long TrobarPremi(int indexnummatch, long[] premis){
         
-        int e =0;
+        long valorPremi = premis[indexnummatch];
         
-        return e;
+        return valorPremi;
     }
     
-            
 }
