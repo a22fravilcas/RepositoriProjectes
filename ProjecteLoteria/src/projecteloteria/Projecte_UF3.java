445ef6b3e7a0f6ee7649a4fa958c2d09409d7377
sonnymccammond <sonnymccammond@192.168.16.210>
2023-02-17 10:35:10 +0100

    uf3 mod (sonny)

--- ProjecteLoteria/src/projecteloteria/Projecte_UF3.java
+++ ProjecteLoteria/src/projecteloteria/Projecte_UF3.java
@@ -6,25 +6,29 @@ import utilities.*;
 
 public class Projecte_UF3 {
 
+    public static String nomFitxer;
+    public static String pathName = Utilities.PATH_FITXER + nomFitxer + Utilities.EXTENSIONS_FITXER_BIN;
+    public static File fitxer = new File(pathName);
     public static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
 
         int anySorteig = scan.nextInt();
-
+        HistorialSorteig(anySorteig);
     }
 
-    public static boolean HistorialSorteig(int anySorteig) {
-        String nomFitxer = (anySorteig) + "";
-        if (true) {
-
-        } else {
-
+    public static void HistorialSorteig(int anySorteig) {
+        
+        nomFitxer = (anySorteig) + "";
+        if (!fitxer.exists()) {
             Utilities.crearFichero(nomFitxer);
+        } else {
+            Utilities.AbrirFicheroEscrituraBinario(nomFitxer, true, true);
+            
         }
 
-        return false;
-
     }
+    
+    
 
 }
